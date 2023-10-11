bbe612bee5db109535089992d1eebb10c0f536d5
terasfly <84103159+terasfly@users.noreply.github.com>
2023-10-11 23:35:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,80 @@
-const taskButton = document.getElementById('task-button')
+// Constants for important HTML elements
+const taskButton = document.getElementById('task-button');
+const tasksList = document.getElementById('tasks');
+const alert = document.querySelector('.alert');
+const yourTaskInput = document.getElementById('your-task');
 
-const tasksList = document.getElementById('tasks')
-const alert = document.querySelector('.alert')
-
-taskButton.addEventListener('click', () => {
-    const yourTask = document.getElementById('your-task').value
-
-    if (yourTask === '') {
-        const boxAlert = document.createElement('div')
-        boxAlert.innerHTML = `
-        <p class="alert-text">Please enter your task!!</p>
-        `
-        alert.appendChild(boxAlert)
-
-    } else {
-        alert.innerHTML = ''
-        addTask(yourTask)
-
-    }
-
-})
-
-// function bigletter(string) {
-//     const toUpper = string.toUpperCase()
-//     console.log(toUpper)
-// }
-// // const vardas = tomas
-// bigletter(...vardas)// Save tasks to localStorage
-function saveTasksToLocalStorage(tasks) {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+// Function to display an alert message
+function displayAlert(message) {
+    const alertBox = document.createElement('div');
+    alertBox.innerHTML = `<p class="alert-text">${message}</p>`;
+    alert.appendChild(alertBox);
 }
 
-// Retrieve tasks from localStorage
-function getTasksFromLocalStorage() {
-    const tasksJSON = localStorage.getItem('tasks');
-    return JSON.parse(tasksJSON) || []; // Return an empty array if no data is found
+// Function to clear the alert
+function clearAlert() {
+    alert.innerHTML = '';
 }
 
-// Use these functions to save and retrieve your tasks
-let tasks = getTasksFromLocalStorage();
-
-// Add a task
-function addTask(yourTask) {
-    tasks.push(yourTask);
-    saveTasksToLocalStorage(tasks);
-}
-
-
-
+// Function to add a task
 function addTask(yourTask) {
     const newTask = document.createElement('div');
     newTask.innerHTML = `
-            <input class="text" name="name" value="${yourTask.toUpperCase()}">
-            <button type="button" class="task-box__button"><i class="fas fa-trash"></i></button>
-        `;
+        <input class="text" name="name" value="${yourTask.toUpperCase()}">
+        <button type="button" class="task-box__button"><i class="fas fa-trash"></i></button>
+    `;
 
-
-
+    // Find the trash button within the new task
     const trashButton = newTask.querySelector('.task-box__button');
 
-    // Add a click event listener to the trash icon button to remove the task
+    // Add a click event listener to the trash button to remove the task
     trashButton.addEventListener('click', () => {
-            // Remove the parent container of the trash icon button, which is the task itself
-            newTask.remove()
-        })
-        //  add new task to taskList
-    tasksList.appendChild(newTask)
-    document.getElementById('your-task').value = ''
+        newTask.remove();
+        updateLocalStorage();
+    });
 
+    // Append the new task to the task list
+    tasksList.appendChild(newTask);
 
+    // Clear the input field
+    yourTaskInput.value = '';
+
+    // Update localStorage with the current task list
+    updateLocalStorage();
 }
+
+// Add a click event listener to the task button
+taskButton.addEventListener('click', () => {
+    const yourTask = yourTaskInput.value.trim(); // Remove leading/trailing whitespace
+
+    if (yourTask === '') {
+        displayAlert('Užrašyk užduoti');
+    } else {
+        clearAlert();
+        addTask(yourTask);
+    }
+});
+
+// Function to update localStorage with the current task list
+function updateLocalStorage() {
+    const taskElements = tasksList.querySelectorAll('div');
+    const taskArray = [];
+    taskElements.forEach((taskElement) => {
+        const taskInput = taskElement.querySelector('.text');
+        taskArray.push(taskInput.value);
+    });
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+}
+
+// Function to load saved tasks from localStorage
+function loadTasksFromLocalStorage() {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (savedTasks) {
+        savedTasks.forEach((task) => {
+            addTask(task);
+        });
+    }
+}
+
+// Load saved tasks from localStorage when the page loads
+window.addEventListener('load', loadTasksFromLocalStorage);
